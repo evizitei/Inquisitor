e4318af5159a7f4d1972e105784503da28bbfc08
evizitei <ethan.vizitei@gmail.com>
2010-12-10 15:39:45 -0600

    skelton commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inquisition::Application.config.secret_token = '43a72b3242e6ec4a81a8d2b7c8c63188eb066cf0c3dbd421e403a5267f3317f57dd062040af9d19ef1789642064881642d4a295e4e73136d4e87368fa34c5cd7'
