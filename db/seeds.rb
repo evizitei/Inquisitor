069737d92e8e82dd61d9a3885744feb89eb0dd42
evizitei <ethan.vizitei@gmail.com>
2010-12-13 20:12:10 -0600

    add bbp seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,3 +69,70 @@ quiz.questions.create!(:id=>10,
                        :prompt=>"Any type of respirator will work in all type of gases and vapors",
                        :correct_answer=>"false",
                        :distractor_1=>"true")
+                       
+                       
+quiz = Quiz.find_or_create_by_name("Blood Bourne Pathogens")
+quiz.update_attributes(:pass_percentage=>75)
+
+quiz.questions.create!(:id=>11,
+                       :prompt=>"The term \"Bloodborne Pathogen\" means:",
+                        :correct_answer=>"Pathogenic microorganisms that are present in human blood and can cause disease in humans",
+                        :distractor_1=>"Viral infectious diseases that are carried strictly in human blood",
+                        :distractor_2=>"Bacterial infectious diseases that are carried strictly in human blood.",
+                        :distractor_3=>"Pathogenic microorganisms that are present in human blood and can cause no diseases in humans.")
+
+quiz.questions.create!(:id=>12,
+                       :prompt=>"The scope of the Bloodborne Pathogens standard is not limited to employees in Healthcare Professions.",
+                        :correct_answer=>"true",
+                        :distractor_1=>"false")
+
+quiz.questions.create!(:id=>13,
+                       :prompt=>"The most common cause of exposure is:",
+                        :correct_answer=>"Needle Sticks",
+                        :distractor_1=>"Blood spilled on surfaces.",
+                        :distractor_2=>"Urine and Fecal Matter contact",
+                        :distractor_3=>"Contact of bodily fluids with mucous membranes.")
+
+quiz.questions.create!(:id=>14,
+                       :prompt=>"Engineered controls to isolate workers from contaminated sharps include:",
+                        :correct_answer=>"All of these",
+                        :distractor_1=>"Sharps boxes",
+                        :distractor_2=>"Needleless Systems",
+                        :distractor_3=>"Self-sheathing needles")
+
+quiz.questions.create!(:id=>15,
+                       :prompt=>"Bending, breaking or cutting off needles is an acceptable alternative to sharps boxes even if they are available.",
+                        :correct_answer=>"false",
+                        :distractor_1=>"true")
+
+quiz.questions.create!(:id=>16,
+                       :prompt=>"Hepatitis B Vaccine is provided free of charge to the members of BCFPD and MO TF-1.",
+                        :correct_answer=>"true",
+                        :distractor_1=>"false")
+
+quiz.questions.create!(:id=>17,
+                       :prompt=>"Members of BCFPD and MO TF-1 cannot decline taking the Hepatitis B Vaccine.",
+                        :correct_answer=>"false",
+                        :distractor_1=>"true")
+
+quiz.questions.create!(:id=>18,
+                       :prompt=>"When dealing with a patient who may have a communicable disease, the appropriate PPE would include:",
+                        :correct_answer=>"All of these",
+                        :distractor_1=>"Gloves (latex or no)",
+                        :distractor_2=>"Eye protection",
+                        :distractor_3=>"Surgical mask or N95 respirator")
+
+quiz.questions.create!(:id=>19,
+                       :prompt=>"Individual member medical records are confidential and kept:",
+                        :correct_answer=>"Accessible only to the member and to the Fire District’s occupational health physician or the members personal physician",
+                        :distractor_1=>"Accessible only to the member. ",
+                        :distractor_2=>"Accessible only to the Fire District’s occupational health physician or the members personal physician.",
+                        :distractor_3=>"All other answers AND Fire District administration")
+
+quiz.questions.create!(:id=>20,
+                       :prompt=>"The best practice for the members to protect themselves from  possible exposure to bloodborne pathogens is/are to:",
+                        :correct_answer=>"all of these",
+                        :distractor_1=>"Practice good hygiene (hand washing)",
+                        :distractor_2=>"Wear appropriate PPE",
+                        :distractor_3=>"A. Receive vaccinations and boosters")
+
