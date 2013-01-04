2d390907e48bbca69f2bc1358389bb5ad941759c
Ethan Vizitei <ethan.vizitei@gmail.com>
2013-01-04 12:33:57 -0600

    add cc for training

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -2,9 +2,9 @@ class StudentMailer < ActionMailer::Base
   default :from => "quizzes-noreply@bcfdmo.com"
 
   def certificate_email(attempt)
-  	@score = attempt.percent_correct
-  	@quizname = attempt.quiz.name
-  	@student = attempt.student.organization_id
-  	mail(:to => attempt.student.email, :subject => "Certificate for #{@quizname}")
+    @score = attempt.percent_correct
+    @quizname = attempt.quiz.name
+    @student = attempt.student.organization_id
+    mail(:to => attempt.student.email, :cc => 'training@bcfdmo.com', :subject => "Certificate for #{@quizname}")
   end
 end
